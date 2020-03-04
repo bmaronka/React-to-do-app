c94ca14d801ff0c12e5d6bbd34797174e7296926
Bartosz Maronka <bartek.maronka@gmail.com>
2020-03-04 12:13:03 +0100

    Add font color if task is important and change display of done tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,18 +1,36 @@
 import React from 'react';
 
 const Task = props => {
-    const { text, date, id } = props.task
+    const { text, date, id, active, important, finishDate } = props.task
+    const importantStyle = {
+        color: 'red',
+    };
 
-    return (
-        <>
-            <p>
-                <strong>{text}</strong> - till <span>{date} </span>
-                <button onClick={() => props.change(id)}>Done</button>
-                <button onClick={() => props.delete(id)}>X</button>
-            </p>
+    if (active) {
+        return (
+            <>
+                <p>
+                    <strong style={important ? importantStyle : null}>{text}</strong> - till <span>{date} </span>
+                    <button onClick={() => props.change(id)}>Done</button>
+                    <button onClick={() => props.delete(id)}>X</button>
+                </p>
 
-        </>
-    );
+            </>
+        );
+    } else {
+        const finishTime = new Date(finishDate).toLocaleString();
+
+        return (
+            <>
+                <p>
+                    <strong>{text}</strong><em>(finish till: {date})</em>
+                    <br />
+                    Done <span>{finishTime}</span>
+                    <button onClick={() => props.delete(id)}>X</button>
+                </p>
+            </>
+        )
+    }
 }
 
 export default Task;
